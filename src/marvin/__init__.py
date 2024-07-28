a03175ca6543088cecbb2dfdf3b1d5b1ef1900e9
usysrc <usysrc@usysrc.dev>
2024-07-28 19:37:53 +0200

    feat: :ambulance: revert back to seperate message

--- src/marvin/__init__.py
+++ src/marvin/__init__.py
@@ -8,40 +8,29 @@ access_token = os.environ["MATRIX_ACCESSTOKEN"]
 room_id = os.environ["MATRIX_ROOMID"]
 message = os.environ["MATRIX_MESSAGE"]
 
+
 async def send_poll():
     client = AsyncClient(homeserver_url, device_id="PollBot")
     client.access_token = access_token
-    
+
+    await send_message(client, "montag")
+    await send_message(client, "dienstag")
+    await send_message(client, "mittwoch")
+    await send_message(client, "donnerstag")
+    await send_message(client, "freitag")
+
+    await client.close()
+
+async def send_message(client, msg):
     await client.room_send(
         room_id,
         "m.room.message",
         {
             "msgtype": "m.text",
-            "body": "Greetings humans!"
+            "body": msg,
         },
         ignore_unverified_devices=True
     )
-
-    poll_content = {
-        "org.matrix.msc3381.poll.start": {
-            "question": {
-                "org.matrix.msc1767.text": "Weekly Gaming Session Poll",
-                "body": "Weekly Gaming Session Poll!",
-                "msgtype": "m.text"
-            },
-            "kind": "org.matrix.msc3381.poll.disclosed",
-            "max_selections": 5,
-            "answers": [
-                {"id": "montag", "org.matrix.msc1767.text": "Montag"},
-                {"id": "dienstag", "org.matrix.msc1767.text": "Dienstag"},
-                {"id": "mittwoch", "org.matrix.msc1767.text": "Mittwoch"},
-                {"id": "donnerstag", "org.matrix.msc1767.text": "Donnerstag"},
-                {"id": "freitag", "org.matrix.msc1767.text": "Freitag"},]
-        }
-    }
-
-    await client.room_send(room_id, "org.matrix.msc3381.poll.start", poll_content)
-    await client.close()
 
 def run_poll():
     asyncio.get_event_loop().run_until_complete(send_poll())
